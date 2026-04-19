bf5f57ab67192f1e834bf5f4acb2c7e2245c05ef
Nithin-127 <nithinmanoj127@gmail.com>
2026-04-19 11:31:59 +0530

    homeshowcase section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import BreadcrumbWrapper from "@/components/BreadcrumbWrapper";
 import Home from "@/components/Home";
+import Homeshowcase from "@/components/about/Homeshowcase";
 import HomeReview from "@/components/about/HomeReview";
 import "./about/about.css";
-
 
 import React from "react";
 
@@ -10,6 +10,8 @@ const page = () => {
   return (
     <>
       <Home />
+      <Homeshowcase />
+      <HomeReview />
     </>
   );
 };
